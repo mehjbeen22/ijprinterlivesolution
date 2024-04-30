61bb50876abeb8035bdda2cf3390cd8208f7bce2
mehjbeen22 <mehjbeen222navgurukul.org>
2024-04-30 16:23:56 +0530

    update

--- src/components/sections/Products.jsx
+++ src/components/sections/Products.jsx
@@ -41,7 +41,7 @@ export default function Products() {
                   >
                     <CardMedia
                       component="img"
-                      sx={{ maxWidth: '50%' }}
+                      // sx={{ maxWidth: '50%' }}
                       image={imageLink}
                       alt="Product"
                     />
