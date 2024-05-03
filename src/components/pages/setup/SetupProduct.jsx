0f92da770473d6495410a622f2259c72d4b13614
mehjbeen22 <mehjbeen222navgurukul.org>
2024-05-03 19:36:22 +0530

    scroll view added

--- src/components/pages/setup/SetupProduct.jsx
+++ src/components/pages/setup/SetupProduct.jsx
@@ -1,8 +1,34 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { printer_series } from './data';
+import { ScrollView } from '@aws-amplify/ui-react';
 
 const SetupProduct = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showError, setShowError] = useState(false);
+  const [hideScrollOnClick, sethideScrollOnClick] = useState(true);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleShowError = () => {
+    setShowError(!showError);
+  };
+
+  const filteredPrinterSeries = printer_series.filter((item) =>
+    item.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  const showScrollView = searchTerm && filteredPrinterSeries.length > 0;
+
+  const selectItem = (item) => {
+    setSearchTerm(item);
+    sethideScrollOnClick(false);
+  };
+
   return (
-    <div className="flex flex-col justify-between  mx-auto  max-w-4xl sm:mt-10 shadow-md p-2 border border-gray-200 rounded-sm ">
+    <div className="flex flex-col justify-between mx-auto max-w-4xl sm:mt-10 shadow-md p-2 border border-gray-200 rounded-sm ">
       <Link
         to="/"
         className="border-1 border-black focus:ring-2 focus:ring-white focus:outline-none font-medium rounded-lg text-sm p-2 w-[10%] mt-4 self-end text-center"
@@ -17,19 +43,40 @@ const SetupProduct = () => {
         <p className="text-center text-lg">
           Search for your product name or select from the list.
         </p>
-        <div className="flex flex-row justify-center items-center w-full">
-          <input
-            type="text"
-            placeholder="Example: TS9030"
-            className=" border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
-          />
-          <Link
-            to="/canonsetup"
-            className="hover:bg-red-600 bg-black text-white  font-medium  text-md px-3 py-2.5 text-center ml-2"
-          >
-            Go
-          </Link>
+        <div className="flex flex-col justify-center items-center w-full">
+          {showError && (
+            <p className="text-red-500">
+              Could not find the product. Enter the product name correctly.
+            </p>
+          )}
+
+          <div>
+            <input
+              type="text"
+              placeholder="Example: TS9030"
+              className="border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+            <Link
+              to={showScrollView ? '/canonsetup' : '/productsetup'}
+              className="hover:bg-red-600 bg-black text-white font-medium text-md px-3 py-2.5 text-center ml-2"
+              onClick={handleShowError}
+            >
+              Go
+            </Link>
+          </div>
+          {hideScrollOnClick && showScrollView && (
+            <ScrollView height="150px" width="290px" border="1px solid #cbd5e1">
+              {filteredPrinterSeries.map((item) => (
+                <div key={item} onClick={() => selectItem(item)}>
+                  <h3 className="hover:bg-gray-300">{item}</h3>
+                </div>
+              ))}
+            </ScrollView>
+          )}
         </div>
+
         <p className="text-base text-blue-400 underline">
           Where to find your product name
         </p>
@@ -41,7 +88,7 @@ const SetupProduct = () => {
             return (
               <li
                 key={index}
-                className="border-2 border-grey-200 ocus:ring-2 focus:ring-white focus:outline-none font-medium rounded-lg p-2 m-2"
+                className="border-2 border-grey-200 focus:ring-2 focus:ring-white focus:outline-none font-medium rounded-lg p-2 m-2"
               >
                 {`➕   ${series}`}
               </li>
@@ -49,7 +96,7 @@ const SetupProduct = () => {
           })}
         </ul>
 
-        <select name="operaValues" className="w-[20%] border-2 border ">
+        <select name="operaValues" className="w-[20%] border-2 border">
           {opareaValues.map((lang, index) => (
             <option key={index}>{lang}</option>
           ))}
